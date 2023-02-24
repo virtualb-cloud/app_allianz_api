4356fdcc01d5c4d6866f3f01befd7d27b64d8ee4
Bizhan Zahedi <79849714+bizhanzhd@users.noreply.github.com>
2023-02-24 12:08:24 +0100

    test

--- app/background_tasks.py
+++ app/background_tasks.py
@@ -4,14 +4,8 @@ from app.ingestion_customers import Customers
 
 def ingestor(df:pd.DataFrame):
 
-    l1 = round(df.shape[0]/2)
-    ingestor = Customers(external_df=df.loc[0:l1, :])
+    ingestor = Customers(external_df=df)
     response = ingestor.run()
     if response: flag =  True
-    else: flag = False
-
-    ingestor = Customers(external_df=df.loc[l1:, :])
-    response = ingestor.run()
-    if response: flag = True
     else: flag = False
 
