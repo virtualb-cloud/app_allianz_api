44ead1fa54e679da9697ebba00b9e81cbd0f70fe
Bizhan Zahedi <79849714+bizhanzhd@users.noreply.github.com>
2023-02-27 18:18:54 +0100

    test

--- app/background_tasks.py
+++ app/background_tasks.py
@@ -4,7 +4,23 @@ import time
 
 def ingestor(df:pd.DataFrame):
 
+    l1 = round(len(df)/20)
+
+    init = 0
+    fin = init + l1
+
+    while fin < len(df):
+        
+        ingestor = Positions()
+        response = ingestor.run(df=df.loc[init:fin, :])
+        if response: flag =  True
+        else: flag = False
+
+        init = fin + 1
+        fin = init + l1
+        time.sleep(5)
+
     ingestor = Positions()
-    response = ingestor.run(df=df)
+    response = ingestor.run(df=df.loc[init:, :])
     if response: flag =  True
     else: flag = False
